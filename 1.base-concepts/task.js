69ca3018a50b886b472a2af654cfd2695e1991d9
Olya Virchenko <olya.lyoka02@gmail.com>
2022-08-16 16:53:44 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,21 +3,29 @@
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
-  let discriminant = b ** 2 - 4 * a * c;
+  let discriminant = b * b - 4 * a * c;
+
+  let x1;
+  let x2;
 
   if (discriminant < 0) {
     arr = [];
-    
-  } else if (discriminant = 0) {
-    arr = [-b / (2*a)];
-    
+
+  } else if (discriminant == 0) {
+    x1 = -b / (2 * a);
+
+    arr = [x1];
+
   } else if (discriminant > 0) {
-    arr = [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
-    
+    x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+    arr = [x1, x2];
   }
 
   return arr; // array
 }
+
 
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
