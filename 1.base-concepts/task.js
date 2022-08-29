4bbded9f375dc8fa8cc76f396826bb4b02e0c056
Olya Virchenko <olya.lyoka02@gmail.com>
2022-08-29 16:42:02 +0300

    Mistakes solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -21,10 +21,21 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
   // код для задачи №2 писать здесь
-const parsed = parseInt(percent, contribution, amount, date);
-Number.isNaN(parsed);
+ parseInt(percent, 10);
+ Number.isNaN(percent);
+
+ parseInt(contribution, 10);
+ Number.isNaN(contribution);
+
+ parseInt(amount, 10);
+ Number.isNaN(amount);
+
+ parseInt(date, 10);
+Number.isNaN(date);
   
- totalAmount  = amount - contribution; 
+ loanBody  = amount - contribution; 
+
+   
 
   let currentMonth = new Date().getMonth();
   let currentYear = new Date().getFullYear();
@@ -33,8 +44,9 @@ Number.isNaN(parsed);
 
   let P = percent / 12 / 100;
 
-  contribution = (amount - contribution) * (P + (P / (((1 + P) ** term) - 1)));
+  oneContribution = (loanBody) * (P + (P / (((1 + P) ** term) - 1)));
 
+  totalAmount = Math.round(oneContribution * term); 
   
   return totalAmount;
 }
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,6 +3,15 @@ function compareArrays(arr1, arr2) {
 
   // Ваш код
 
+  for (let i = 0; i < arr1.length; i += 1) {
+    if (arr1[i] === arr2[i]) {
+      //сравить не только типы данных но и значения
+      result = true;
+    } else {
+      result = false;
+    }
+  }
+
   return result; // boolean
 }
 
@@ -10,6 +19,8 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+
+  let resultArr = arr.filter(item => item > 0).filter(element => element % 3 === 0).map(element => element * 100)
 
   return resultArr; // array
 }
