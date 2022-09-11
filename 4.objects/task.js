1b74e50b48b99664ee6516ee3e1342212e97b874
Olya Virchenko <olya.lyoka02@gmail.com>
2022-09-11 13:09:18 +0300

    First task solution

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,8 +5,8 @@ function Student(name, gender, age) {
   this.age = age;
 }
 
-let student1 = new Student("Жанна", "жен", 21);
-let student2 = new Student("Игорь", "муж", 19);
+let student9 = new Student("Жанна", "жен", 21);
+let student10 = new Student("Игорь", "муж", 19);
 
 Student.prototype.setSubject = function(subjectName) {
   //ваш код
@@ -15,17 +15,65 @@ Student.prototype.setSubject = function(subjectName) {
 
 // ваш код для остальных методов
 
-Student.prototype.addMark = function(addMark) {
+Student.prototype.addMark = function(mark) {
   //ваш код
-  this.mark = addMark;
+  this.mark = mark;
 
   if(this.marks === undefined){ 
-    let this.marks = [];
-    this.marks.push(this.mark);
+    this.marks = [this.mark];
     } else {
     this.marks.push(this.mark);
     } 
 }
+
+
+Student.prototype.addMarks() = function(mark1, mark2, mark3, ...args) {
+  //ваш код
+ this.marks = (...args);
+ this.marks.push(args);
+} 
+
+
+
+Student.prototype.getAverage() = function(average) {
+  let sum = 0;
+  for (let i = 0; i < Student.keys(this.marks).length; i += 1) {
+     sum += this.marks[Student.keys(this.marks)[i]].reduce((acc, item) => item + acc, 0);
+   } 
+  return console.log("Средний балл по всем предметам" + (sum / Student.keys(this.marks).length));
+}
+
+Student.prototype.exclude() = function(reason) {
+  
+  delete Student.prototype.setSubject;
+  delete Student.prototype.addMark;
+
+   this.excluded = reason;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
