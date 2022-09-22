00f3e38d7b9b36509cebce4b4e71b216f20bbcbf
Olya Virchenko <olya.lyoka02@gmail.com>
2022-09-22 13:44:56 +0300

    Initial coomit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,18 +1,20 @@
 //Задача 1
 
+function parseCount(el) {
+	
+  if (Number.parseInt(el) === NaN) {
+    throw new Error('Невалидное значение');
+  }
+  	return Number.parseInt(el);
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
+function validateCount(el) {
+  try {
+  	parseCount(el)
+  } catch (error) {
+     return error;
+  }
+}
 
 
 
@@ -23,12 +25,12 @@
 
 class Triangle {
 	constructor (a, b, c) {
-		func(a, b,c) {
+		func(a, b, c) {			
 			if (a + b < c || b + c < a || a + c < b) {
-				//исключение "Треугольник с такими сторонами не существует".
+	  		throw new Error("Треугольник с такими сторонами не существует")
 			}
 		}
-	
+	}
 	getPerimeter() {
 		let perimeter = a + b + c;
 		return console.log("Периметр треуголиника: " + perimeter)
@@ -36,8 +38,47 @@ class Triangle {
 
 	getArea() {
 		let halfPerimeter = perimeter / 2;
+		let area = Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+		return area.toFixed(3);
+	}
+
+
+}
+	
+function getTriangle(a, b, c) {
+	try {
+		let triangle = new Triangle(a, b, c);
+		return triangle
+	} catch {
+		 return triangle.getArea(), triangle.getPerimeter(), ("Ошибка! Треугольник не существует");
+		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
